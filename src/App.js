ab50cb8fd995a3d3388a05c97aadab1d659320e6
Reiwhite <chononecorn02@gmail.com>
2025-02-13 15:03:58 +0700

    Batazero

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <header className="App-header">
       <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/Maru-Buffet" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
